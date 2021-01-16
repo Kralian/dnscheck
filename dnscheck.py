5e21319e6e7b586646ad444376432394555f6cda
Anders Mundt Due <amd@pd-house.dk>
2021-01-17 00:14:56 +0100

    ia stringfied default

--- dnscheck.py
+++ dnscheck.py
@@ -47,7 +47,7 @@ class ShellResolver(BaseResolver):
         #print(f"qtype: {request.q.qtype}")
         if qname in ['whoami','myip']:
             rqt = QTYPE.TXT
-            rqd = f"I see you from {ia}"
+            rqd = f"I see you from {str(ia)}"
             if request.q.qtype in [QTYPE.A,QTYPE.AAAA]:
                 if ia.version is 6:
                     rqt = request.q.qtype
